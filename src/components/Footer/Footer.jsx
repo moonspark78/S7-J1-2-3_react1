3a0797d542d73703d94ea998bef3f3f9c0c1f65a
Omar <jabour.omar@hotmail.com>
2025-09-22 11:40:19 +0200

    :art: done activite 4 & 5  :art:

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+  return (
+    <div>
+        <hr/>
+        <p>Copyright Mexican Food 2024</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="Logo" />
+    </div>
+  )
+}
+
+export default Header
