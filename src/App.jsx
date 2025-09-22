4bba113d9ab9ff840a5ed151c9a67585772fe1ee
Omar <jabour.omar@hotmail.com>
2025-09-22 14:07:26 +0200

    :art: done activite 6 fix2 ajout de main  :art:

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,30 @@ import Dish from "./components/Dish/Dish";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
-
 function App() {
-
   return (
     <>
-      <Header/>
-      <Dish 
-        name="Tacos à l’unité"
-        price={3}
-        image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-      />
-      <Dish
-        name="Enchiladas"
-        price={12}
-        image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-      />
-      <Dish
-        name="Mole poblano"
-        price={15}
-        image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-      />
-      <Footer/>
+      <Header />
+      <main>
+        <Dish
+          name="Tacos à l’unité"
+          price={3}
+          image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
+        />
+        <Dish
+          name="Enchiladas"
+          price={12}
+          image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
+        />
+        <Dish
+          name="Mole poblano"
+          price={15}
+          image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
+        />
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
