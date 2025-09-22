53287b5325b4aa1ed4aaec0f0c8d3e740b98ae37
Omar <jabour.omar@hotmail.com>
2025-09-22 11:55:54 +0200

    :art: done activite 4 & 5 fix-1  :art:

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,19 @@
-
 const Header = () => {
   return (
     <div>
-        <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="Logo-site" />
+      <img
+        src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
+        alt="Logo-site"
+      />
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Accueil</a>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
