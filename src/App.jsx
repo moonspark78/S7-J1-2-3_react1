c209940ea539c1a3a756ce64d91cac71a575de02
Omar <jabour.omar@hotmail.com>
2025-09-23 11:42:46 +0200

    :art: done activité 9:sparkles::sparkles:

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
                 name="Tacos à l’unité"
                 price={3}
                 image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
+                isNew={true}
               />
             </Col>
             <Col md={4}>
@@ -23,6 +24,7 @@ function App() {
                 name="Enchiladas"
                 price={12}
                 image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
+                isNew={false}
               />
             </Col>
             <Col md={4}>
@@ -30,6 +32,7 @@ function App() {
                 name="Mole poblano"
                 price={15}
                 image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
+                isNew={false}
               />
             </Col>
           </Row>
--- src/components/Dish/Dish.jsx
+++ src/components/Dish/Dish.jsx
@@ -1,12 +1,21 @@
 import Card from "react-bootstrap/Card";
+import Badge from "react-bootstrap/Badge";
 import "./dish.scss";
 
-const Dish = ({ name, price, image }) => {
+const Dish = ({ name, price, image, isNew }) => {
   return (
     <Card>
+      <div className="image-container">
       <Card.Img variant="top" src={image} alt={name} />
+          {isNew && (
+            <Badge bg="primary" className="badge">
+              Nouveau
+            </Badge>
+          )}
+      </div>
       <Card.Body>
-        <Card.Title>{name}</Card.Title>
+        <Card.Title>{name}
+        </Card.Title>
         <Card.Text>
           {price}€
         </Card.Text>
