a3e017c70829af260be996cd21f0d17bb7ad80d1
Omar <jabour.omar@hotmail.com>
2025-09-23 15:03:37 +0200

    :art: done activité 13 fix1 :sparkles::sparkles:

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Dish from "./components/Dish/Dish";
 import Footer from "./components/Footer/Footer";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import Header from "./components/Header/Header";
 import { Container, Row, Col } from "react-bootstrap";
 import { useState } from "react";
@@ -9,54 +9,58 @@ import "./App.scss";
 function App() {
   const [showNewOnly, setShowNewOnly] = useState(false);
   const handleShowNewOnly = () => {
-  setShowNewOnly(!showNewOnly);
-};
+    setShowNewOnly(prevShowNewOnly => !prevShowNewOnly);
+  };
   const dishes = [
     {
-    name: "Tacos à l’unité",
-    price: 3,
-    image: "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
-    isNew: true,
-    stock:12
-  },
-  {
-    name: "Enchiladas",
-    price: 12,
-    image: "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
-    isNew: false,
-    stock:0
-  },
-  {
-    name: "Mole poblano",
-    price: 15,
-    image: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
-    isNew: false,
-    stock:5
-    }
+      name: "Tacos à l’unité",
+      price: 3,
+      image:
+        "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
+      isNew: true,
+      stock: 12,
+    },
+    {
+      name: "Enchiladas",
+      price: 12,
+      image:
+        "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
+      isNew: false,
+      stock: 0,
+    },
+    {
+      name: "Mole poblano",
+      price: 15,
+      image:
+        "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
+      isNew: false,
+      stock: 5,
+    },
   ];
-    
-    const filteredDishes = dishes.filter(dish => dish.stock > 0 && (!showNewOnly || dish.isNew));
 
+  const filteredDishes = dishes.filter(
+    (dish) => dish.stock > 0 && (!showNewOnly || dish.isNew)
+  );
 
   return (
     <>
       <Header />
       <main>
         <Container>
-        <Button variant="secondary" onClick={handleShowNewOnly}>
-          {showNewOnly ? "Voir tous les plats" : "Nouveautés uniquement"}
-        </Button>
+          <Button variant="secondary" onClick={handleShowNewOnly}>
+            {showNewOnly ? "Voir tous les plats" : "Nouveautés uniquement"}
+          </Button>
           <Row>
-              {filteredDishes.map((dish, index) => (
-                <Col md={4} key={index} >
-                <Dish 
-                name={dish.name}
-                price={dish.price}
-                image={dish.image}
-                isNew={dish.isNew}
+            {filteredDishes.map((dish, index) => (
+              <Col md={4} key={index}>
+                <Dish
+                  name={dish.name}
+                  price={dish.price}
+                  image={dish.image}
+                  isNew={dish.isNew}
                 />
-                </Col>
-              ))}
+              </Col>
+            ))}
           </Row>
         </Container>
       </main>
--- src/components/Dish/Dish.jsx
+++ src/components/Dish/Dish.jsx
@@ -1,29 +1,27 @@
 import Card from "react-bootstrap/Card";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import Badge from "react-bootstrap/Badge";
 
 import "./dish.scss";
 
 const Dish = ({ name, price, image, isNew }) => {
-
   const handleClick = (dishNanme) => {
-      alert(`Le plat ${dishNanme} est maintenant dans votre panier`)
-    }
+    alert(`Le plat ${dishNanme} est maintenant dans votre panier`);
+  };
   return (
     <Card>
       <Card.Img variant="top" src={image} alt={name} />
-          {isNew && (
-            <Badge bg="primary" className="badge">
-              Nouveau
-            </Badge>
-          )}
+      {isNew && (
+        <Badge bg="primary" className="badge">
+          Nouveau
+        </Badge>
+      )}
       <Card.Body>
-        <Card.Title>{name}
-        </Card.Title>
-        <Card.Text>
-          {price}€
-        </Card.Text>
-        <Button variant="primary" onClick={() =>handleClick(name)}>Ajouter au panier</Button>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>{price}€</Card.Text>
+        <Button variant="primary" onClick={() => handleClick(name)}>
+          Ajouter au panier
+        </Button>
       </Card.Body>
     </Card>
   );
