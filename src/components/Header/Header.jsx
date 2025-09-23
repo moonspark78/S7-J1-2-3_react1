5aa4d2fff636d9a087dc79a4cf0aa807be7d567c
Omar <jabour.omar@hotmail.com>
2025-09-23 10:49:56 +0200

    :art: activité 8

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import "./Header.scss";
+import logo from "../../assets/logo.webp";
 
 const Header = () => {
   return (
@@ -8,7 +9,7 @@ const Header = () => {
         <Container>
           <Navbar.Brand href="/">
             <img
-              src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
+              src={logo}
               alt="Un mexicain qui joue de la guitare"
               height="40"
             />
