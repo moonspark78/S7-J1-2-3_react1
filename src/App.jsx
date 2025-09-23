9d6c057d0c80c9ec2ffd5558879de971f2b74819
Omar <jabour.omar@hotmail.com>
2025-09-23 12:19:47 +0200

    :art: done activité 10 :sparkles::sparkles:

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,42 @@ import { Container, Row, Col } from "react-bootstrap";
 import "./App.scss";
 
 function App() {
+  const dishes = [
+    {
+    name: "Tacos à l’unité",
+    price: 3,
+    image: "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
+    isNew: true
+  },
+  {
+    name: "Enchiladas",
+    price: 12,
+    image: "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
+    isNew: false
+  },
+  {
+    name: "Mole poblano",
+    price: 15,
+    image: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
+    isNew: false
+  }
+  ];
   return (
     <>
       <Header />
       <main>
         <Container>
           <Row>
-            <Col md={4}>
-              <Dish
-                name="Tacos à l’unité"
-                price={3}
-                image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-                isNew={true}
-              />
-            </Col>
-            <Col md={4}>
-              <Dish
-                name="Enchiladas"
-                price={12}
-                image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-                isNew={false}
-              />
-            </Col>
-            <Col md={4}>
-              <Dish
-                name="Mole poblano"
-                price={15}
-                image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-                isNew={false}
-              />
-            </Col>
+              {dishes.map((dish, index) => (
+                <Col md={4} key={index} >
+                <Dish 
+                name={dish.name}
+                price={dish.price}
+                image={dish.image}
+                isNew={dish.isNew}
+                />
+                </Col>
+              ))}
           </Row>
         </Container>
       </main>
